0f3118b8a845db431e214e700f7a35670d7ebfe9
Sight100 <ccino4sight@gmail.com>
2023-02-20 03:12:27 -0800

    Print with put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studiio.h>
+
+/**
+ * Use the function puts to print the textt
+ * and return the value 0
+*/
+
+int main()
+{
+	put("\"Programming is like building a multilingual puzzle");
+	retun 0;
+}
