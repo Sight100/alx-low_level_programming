904b1834077e95676df95465ed1445d5f4f2a890
Sight100 <ccino4sight@gmail.com>
2023-02-20 03:47:13 -0800

    Print with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,8 @@
 
 /**
  * main-Use the function puts to print the textt
- * 
  * Return: 0 for succesful exit
 */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
