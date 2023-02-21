b6037790c83e5fb7a57d5933ad3e6c930c28081a
Sight100 <ccino4sight@gmail.com>
2023-02-20 19:49:12 -0800

    Print with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main-Use the function puts to print the textt
+ * Return: 0 for succesful exit
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
